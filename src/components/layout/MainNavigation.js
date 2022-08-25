e976421c4b1c539f69b4f706a5ce6848592e204b
jdm79 <james.malvern@gmail.com>
2022-08-25 18:11:05 +0100

    add max's excellent scoped css modules

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,26 +2,27 @@ import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-    return <header>
-        <div>ngfbeers</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>Home Page</Link>
-                </li>
-                <li>
-                    <Link to='/companies'>Companies Page</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-                
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>ngfbeers</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home Page</Link>
+                    </li>
+                    <li>
+                        <Link to='/companies'>Companies Page</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNavigation;
