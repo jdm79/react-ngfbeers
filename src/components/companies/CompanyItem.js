69a6b96ab548ccf08ef1fcfa3d842c3a65cea1a7
jdm79 <james.malvern@gmail.com>
2022-08-25 22:14:37 +0100

    add children props to the Card ui component

--- src/components/companies/CompanyItem.js
+++ src/components/companies/CompanyItem.js
@@ -1,22 +1,23 @@
-import classes from './CompanyItem.module.css'
+import Card from '../ui/Card';
+import classes from './CompanyItem.module.css';
 
 function CompanyItem(props) {
     return (
         <li className={classes.item}>
-            <div className={classes.image}>
-                <img src={props.image} alt=""/>
-            </div>
-            <div className={classes.content}>
-                <h3>{props.title}</h3>
-                <p>{props.description}</p>
-            </div>
-            <div className={classes.actions}>
-                <button>To favourites</button>
-            </div>
+            <Card>
+                <div className={classes.image}>
+                    <img src={props.image} alt=""/>
+                </div>
+                <div className={classes.content}>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </div>
+                <div className={classes.actions}>
+                    <button>To favourites</button>
+                </div>
+            </Card>
         </li>
     )
-    
-   
 }
 
 export default CompanyItem;
